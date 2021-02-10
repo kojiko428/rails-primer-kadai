ac3aaf237efbad84d61100feed79e9b6b1d3aa0a
koji yamamoto <koji_ko428@yahoo.co.jp>
2021-02-10 07:04:50 +0000

    画面移動リンク（show→post）の設置

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,8 +6,18 @@ class PostsController < ApplicationController
    @post=Post.new
   end
   def create
-   Post.create(post_params)
-   redirect_to new_post_path
+   @post =Post.new(post_params)
+
+    if @post.save
+       # 一覧画面へ遷移して"ブログを作成しました！"とメッセージを表示します。
+       redirect_to blogs_path, notice: "投稿を作成しました！"
+     else
+       # 入力フォームを再描画します。
+       render :new
+     end
+
+   # Post.create(post_params)
+   # redirect_to new_post_path
   end
   def show
   @post = Post.find(params[:id])
