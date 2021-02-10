8a730ab55ec4633bca8bd921a48f97ae66a06c90
koji yamamoto <koji_ko428@yahoo.co.jp>
2021-02-10 04:43:39 +0000

    create(post保存)の簡易設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,11 @@ class PostsController < ApplicationController
   def index
   end
   def new
+   @post=Post.new
+  end
+  def create
+   Post.create(content: params[:post][:content])
+
   end
 
 end
