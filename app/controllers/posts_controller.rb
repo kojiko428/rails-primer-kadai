25d27edae86430d5f6357c6d36e04a9d9541baaf
koji yamamoto <koji_ko428@yahoo.co.jp>
2021-02-10 06:22:39 +0000

    投稿詳細画面（show）の設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,10 @@ class PostsController < ApplicationController
    Post.create(post_params)
    redirect_to new_post_path
   end
+  def show
+  @post = Post.find(params[:id])
+  end
+
 
   private
   def post_params
